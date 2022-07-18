3166e2612336d112c672c2cb89407e768ea42db5
CubeMattia <95407151+CubeMattia@users.noreply.github.com>
2022-07-18 15:54:48 +0000

    Deploying to gh-pages from @ CUBE-CONSULTANTS/Sir---B2B@604c1d1a69195d02f53fa4c2bbcd57eb711cc310 🚀

--- resources/sap/ui/Global-dbg.js
+++ resources/sap/ui/Global-dbg.js
@@ -77,7 +77,7 @@ sap.ui.define([
 		 */
 		version: "1.96.0",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20220715-1029" }
+		buildinfo : { lastchange : "", buildtime : "20220718-1553" }
 	});
 
 	var oCfgData = window["sap-ui-config"] || {};
